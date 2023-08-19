3e6827633e105c3ffe42974f9517455fe00d8bef
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-20 04:37:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,31 +72,26 @@ async def perform_imdb_search(client, message):
                 logging.info("Callback query received.")
 
                 try:
-                    mongo_client = MongoClient(DATABASE_URI)
-                    db = mongo_client['TelegramBot']
-                    collection = db['TelegramBot']
-
-                    # Search for movies and web series
-                    similar_titles = collection.find({"$or": [{"file_name": {"$regex": title, "$options": "i"}}, {"file_type": "TV Show", "title": {"$regex": title, "$options": "i"}}]})
-
-                    if similar_titles.count() > 0:
-                        logging.info("Found similar titles for '{}'.".format(title))
-                        await auto_filter(client, title) 
+                    # First, we search IMDb
+                    imdb_search_results = await get_imdb_search_results(title)
+                    if imdb_search_results:
+                        logging.info("Found results for '{}' on IMDb.".format(title))
+                        # If we find results on IMDb, we don't need to search the database
+                        reply_message = "Here are the results for '{}' on IMDb:\n".format(title)
+                        for result in imdb_search_results:
+                            reply_message += "{} ({})\n".format(result['title'], result['year'])
+                            reply_message += "Select the title you want to search by autofilter: "
+                        logging.info("Sending message: {}".format(reply_message))
+                        # We await autofilter to get the title
+                        title = await auto_filter(client, title)
+                        logging.info("Title received from autofilter: {}".format(title))
+                        await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
                     else:
-                        logging.info("No similar titles found for '{}'.".format(title))
-                        # Your IMDb search code
-                        imdb_search_results = await get_imdb_search_results(title)
-                        if imdb_search_results:
-                            reply_message = "Here are the results for '{}' on IMDb:\n".format(title)
-                            for result in imdb_search_results:
-                                reply_message += "{} ({})\n".format(result['title'], result['year'])
-                                reply_message += "Select the title you want to search by autofilter: "
-                            logging.info("Sending message: {}".format(reply_message))
-                            await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
-                        else:
-                            reply_message = f"#Requested_ver {title} ."
-
-                            await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
+                        logging.info("No results found for '{}' on IMDb.".format(title))
+                        # We don't need to search the database because autofilter can do it
+                        reply_message = "No results found in IMDb. Let me autofilter it for you."
+                        logging.info("Sending message: {}".format(reply_message))
+                        await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
                 except Exception as e:
                     logging.error(f"An error occurred: {e}")
 
@@ -105,6 +100,7 @@ async def perform_imdb_search(client, message):
         else:
             suggestion_message = "No results found for '{}'.".format(search_text)
             await message.reply_text(suggestion_message)
+
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
