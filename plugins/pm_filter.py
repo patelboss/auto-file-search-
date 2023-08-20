34b2c26eaab56d233ec41aed6b53c467ac5e8c70
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-20 16:25:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -52,41 +52,53 @@ async def give_filter(client, message):
     except Exception as e:
         logging.error("An error occurred: %s", e)     
 
+
 async def perform_imdb_search(client, message):
     search_text = message.text
+ #   word_count = len(re.findall(r'\w+', search_text))
     logging.info("Received message from user: %s", message.text)
+
+  #  if word_count < 20:
+ #      inline_keyboard = perform_imdb_search(client, message)   
     
-    inline_keyboard = await get_imdb_search(search_text)
-    
-    if inline_keyboard:
-        keyboard_markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
-        await callback_handler(client, message, keyboard_markup)
-    else:
-        suggestion_message = "No results found for '{}'.".format(search_text)
-        await message.reply_text(suggestion_message)
+     #    if inline_keyboard:
+            # Create InlineKeyboardMarkup object
+  #          keyboard_markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+            
+            async def callback_handler(client, query):
+                title = query.text
 
-async def callback_handler(client, query, keyboard_markup):
-    title = query.text
+                logging.info("Callback query received.")
 
-    logging.info("Callback query received.")
+                try:
+                    # First, we search IMDb
+                    imdb_search_results = await get_imdb_search_results(title)
+                    if imdb_search_results:
+                        logging.info("Found results for '{}' on IMDb.".format(title))
+                        # If we find results on IMDb, we don't need to search the database
+                        reply_message = "Here are the results for '{}' on IMDb:\n".format(title)
+                        for result in imdb_search_results:
+                            reply_message += "{} ({})\n".format(result['title'], result['year'])
+                            reply_message += "Select the title you want to search by autofilter: "
+                        logging.info("Sending message: {}".format(reply_message))
+                        # We await autofilter to get the title
+                        title = await auto_filter(client, title)
+                        logging.info("Title received from autofilter: {}".format(title))
+                        await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
+                    else:
+                        logging.info("No results found for '{}' on IMDb.".format(title))
+                        # We don't need to search the database because autofilter can do it
+                        reply_message = "No results found in IMDb. Let me autofilter it for you."
+                        logging.info("Sending message: {}".format(reply_message))
+                        await query.message.edit_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
+                except Exception as e:
+                    logging.error(f"An error occurred: {e}")
 
-    try:
-        imdb_search_results = await get_imdb_search_results(title)
-        if imdb_search_results:
-            logging.info("Found results for '{}' on IMDb.".format(title))
-            reply_message = "Here are the results for '{}' on IMDb:\n".format(title)
-            for result in imdb_search_results:
-                reply_message += "{} ({})\n".format(result['title'], result['year'])
-            logging.info("Sending message: {}".format(reply_message))
-            await query.reply_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
-        else:
-            logging.info("No results found for '{}' on IMDb.".format(title))
-            reply_message = "No results found in IMDb for '{}'. Let me autofilter it for you.".format(title)
-            logging.info("Sending message: {}".format(reply_message))
-            await query.reply_text(reply_message, reply_markup=keyboard_markup, disable_web_page_preview=True)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
+            # Properly await callback_handler
+            await callback_handler(client, query)
+     #   else:
+          #  suggestion_message = "No results found for '{}'.".format(search_text)
+           # await message.reply_text(suggestion_message)
 async def get_imdb_search(search_text):
     ia = IMDb()
     search_results = ia.search_movie(search_text)
