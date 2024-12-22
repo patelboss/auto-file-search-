4bae29b33b5a3db66ee69fdc86ca328fe42e09a0
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-23 01:57:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-SUPPORT_GROUP_ID = environ.get('SUPPORT_GROUP_ID', '-1001860020592' )
+SUPPORT_CHAT_ID = environ.get('SUPPORT_CHAT_ID', '-1001860020592' )
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "LazyDeveloper")
