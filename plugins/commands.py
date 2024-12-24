e85d545c08e02e4c08f87bd4769382c55cd76980
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-24 15:39:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,7 @@ from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, clean_file_name
 from utils import *
-from verify import *
+#from verify import *
 from database.connections_mdb import active_connection
 import re
 from info import *
