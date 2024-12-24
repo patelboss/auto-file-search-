81c7d3f337e07e5b2033e434180b2745c0058975
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-24 15:49:57 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -8,6 +8,7 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.users = self.db.uersz
         self.grp = self.db.groups
 
 
