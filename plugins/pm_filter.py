98b74dada5edb828407b7edb3586efedb2ff2e8b
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-26 16:12:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -102,7 +102,7 @@ async def give_filter(client, message):
 
         # If not in the support chat, execute manual and auto-filter logic
         #logger.info(f"User is not in the support chat (chat_id: {chat_id}). Executing filters.")
-        await message.reply_text("<b>This Group Ban Anytime so Join Another Private Group.</b> <i> Link : https://t.me/+13JZ5BMiiSM4ZmE1 </i>\n Wait 10 second Bot is finding Movie", disable_web_preview=True)
+        await message.reply_text("<b>This Group Ban Anytime so Join Another Private Group.</b> <i> Link : https://t.me/+13JZ5BMiiSM4ZmE1 </i>\n Wait 10 second Bot is finding Movie", disable_web_page_preview=True)
         await manual_filters(client, message)
         await auto_filter(client, message)
 
