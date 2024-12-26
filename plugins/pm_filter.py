1c93c1e40099633a9bae22b340264a917384a282
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-27 01:46:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -920,7 +920,7 @@ async def advantage_spell_chok(client, msg):
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE
-    ).strip() + " movie"
+    ).strip()
 
     logger.info(f"Processed query: {query}")
 
