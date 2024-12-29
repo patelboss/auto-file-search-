e22eb0b06eec70d9d95ec9c4ad9ee539fba9ac53
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-29 23:33:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,7 +20,7 @@ from database.batch_filedb import fetch_file_by_link, get_batch_by_id, save_batc
 import base64
 from variables import VERIFY, VERIFY_TUTORIAL
 logger = logging.getLogger(__name__)
-
+import builtins
 BATCH_FILES = {}
 STREAM_MODE = "False"
 #VERIFY = "False"
