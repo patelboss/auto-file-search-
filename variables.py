a46305e181ee644b0c90c1be05561bea33c2e75f
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-29 22:20:00 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -21,7 +21,7 @@ VERIFY_SND_SHORTLINK_URL = config.get('VERIFY_SND_SHORTLINK_URL') if config.get(
 
 VERIFY_SND_SHORTLINK_API = config.get('VERIFY_SND_SHORTLINK_API') if config.get('VERIFY_SND_SHORTLINK_API') else environ.get('VERIFY_SND_SHORTLINK_API', '')
 
-VERIFY_TUTORIAL = config.get('VERIFY_TUTORIAL') if config.get('VERIFY_TUTORIAL') else environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
+VERIFY_TUTORIAL = config.get('VERIFY_TUTORIAL') if config.get('VERIFY_TUTORIAL') else environ.get('VERIFY_TUTORIAL', 'https://t.me/Filmykeedha/394')
 
 CUSTOM_FILE_CAPTION = config.get("CUSTOM_FILE_CAPTION") if config.get("CUSTOM_FILE_CAPTION") else environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 
