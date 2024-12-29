39f8c1d3c6f804e4821c2c120cedecf4b9919dde
patelboss <83952959+patelboss@users.noreply.github.com>
2024-12-30 00:44:45 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -145,7 +145,7 @@ async def who_is(client, message):
         )
     await status_message.delete()
 
-@Client.on_message(filters.command(["indb"]))
+@Client.on_message(filters.command(["imdb"]))
 async def imdb_search(client, message):
     if ' ' in message.text:
         k = await message.reply('Searching ImDB')
@@ -166,7 +166,7 @@ async def imdb_search(client, message):
     else:
         await message.reply('Give me a movie / series Name')
 
-@Client.on_callback_query(filters.regex('^indb'))
+@Client.on_callback_query(filters.regex('^imdb'))
 async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     i, movie = quer_y.data.split('#')
     imdb = await get_poster(query=movie, id=True)
