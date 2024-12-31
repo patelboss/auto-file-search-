8938602559a8e92ba67690116e6f76ab08a66000
patelboss <83952959+patelboss@users.noreply.github.com>
2025-01-01 00:01:16 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -23,8 +23,8 @@ VERIFY_SND_SHORTLINK_API = config.get('VERIFY_SND_SHORTLINK_API') if config.get(
 print(f"VERIFY_SND_SHORTLINK_API = {VERIFY_SND_SHORTLINK_API}")
 VERIFY_TUTORIAL = config.get('VERIFY_TUTORIAL') if config.get('VERIFY_TUTORIAL') else environ.get('VERIFY_TUTORIAL', 'https://t.me/Filmykeedha/394')
 print(f"VERIFY_TUTORIAL = {VERIFY_TUTORIAL}")
-CUSTOM_FILE_CAPTION = config.get("CUSTOM_FILE_CAPTION") if config.get("CUSTOM_FILE_CAPTION") else environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-print(f"CUSTOM_FILE_CAPTION = {CUSTOM_FILE_CAPTION}")
+#CUSTOM_FILE_CAPTION = config.get("CUSTOM_FILE_CAPTION") if config.get("CUSTOM_FILE_CAPTION") else environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+#print(f"CUSTOM_FILE_CAPTION = {CUSTOM_FILE_CAPTION}")
 AUTH_CHANNELS = config.get("AUTH_CHANNELS").split(",") if config.get("AUTH_CHANNELS") else environ.get(AUTH_CHANNELS).split(",")
 #environ.get("AUTH_CHANNELS", "").split(",") if config.get("AUTH_CHANNELS") else []
 print(f"AUTH_CHANNELS = {AUTH_CHANNELS}")
@@ -33,3 +33,5 @@ print(f"DLTTM: {config.get('DLTTM')}")
 print(f"DLTTM = {DLTTM}")
 WELCOME_VIDEO_ID = config.get("WELCOME_VIDEO_ID") if config.get("WELCOME_VIDEO_ID") else environ.get("MELCOW_VID", "BAACAgQAAxkBAAEWWw5nXJ_bgRy9MY3ZNxpLzbIaysGuswAC2hoAAuLv4VIyB40_JD42Hh4E")
 print(f"WELCOME_VIDEO_ID = {WELCOME_VIDEO_ID}")
+CUSTOM_FILE_CAPTION = f'{script.CAPTION}'
+print(f"CUSTOM_FILE_CAPTION = {CUSTOM_FILE_CAPTION}")
