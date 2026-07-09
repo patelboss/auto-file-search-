80991fdad12666f817751c307324a34ebff4c6f8
patelboss <83952959+patelboss@users.noreply.github.com>
2026-07-09 20:36:12 +0530

    Fix AUTH_CHANNELS environment variable fallback

--- variables.py
+++ variables.py
@@ -25,7 +25,9 @@ VERIFY_TUTORIAL = config.get('VERIFY_TUTORIAL') if config.get('VERIFY_TUTORIAL')
 print(f"VERIFY_TUTORIAL = {VERIFY_TUTORIAL}")
 #CUSTOM_FILE_CAPTION = config.get("CUSTOM_FILE_CAPTION") if config.get("CUSTOM_FILE_CAPTION") else environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 #print(f"CUSTOM_FILE_CAPTION = {CUSTOM_FILE_CAPTION}")
-AUTH_CHANNELS = config.get("AUTH_CHANNELS").split(",") if config.get("AUTH_CHANNELS") else environ.get(AUTH_CHANNELS).split(",")
+AUTH_CHANNELS = config.get("AUTH_CHANNELS").split(",") if config.get("AUTH_CHANNELS") else environ.get("AUTH_CHANNELS", "").split(",")
+
+#AUTH_CHANNELS = config.get("AUTH_CHANNELS").split(",") if config.get("AUTH_CHANNELS") else environ.get(AUTH_CHANNELS).split(",")
 #environ.get("AUTH_CHANNELS", "").split(",") if config.get("AUTH_CHANNELS") else []
 print(f"AUTH_CHANNELS = {AUTH_CHANNELS}")
 DLTTM = int(config.get("DLTTM")) if config.get("DLTTM") else int(environ.get("DLTTM", "4200"))
